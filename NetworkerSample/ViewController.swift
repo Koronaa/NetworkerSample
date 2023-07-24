df1f78f4509c74648360cf4e2794bbef95a1e4e6
Koronä <11703410+Koronaa@users.noreply.github.com>
2023-07-24 14:12:54 +0800

    Implements UI

--- NetworkerSample/ViewController.swift
+++ NetworkerSample/ViewController.swift
@@ -11,9 +11,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func getUsersButtonOnTapped(_ sender: Any) {
+        //TODO: 
+    }
+    
 
 }
 
